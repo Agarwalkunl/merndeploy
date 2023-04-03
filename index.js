d7652b8fa563e2f55acf89d8e662b562e03e883d
kunal <you@example.com>
2023-04-03 12:57:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const env = require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+const port = process.env.port || 1000;
+let { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.DATABASE);
+let re, db, col;
+async function ged() {
+  re = await client.connect();
+  db = re.db("jk");
+  col = db.collection("as");
+
+  console.log("connect");
+}
+ged();
+app.get("/read", async (req, resp) => {
+  let resuu = await col.find().toArray();
+  resp.send(resuu);
+});
+
+app.listen(port, () => {
+  console.log("server start");
+});
